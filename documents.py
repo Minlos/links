01eb516017c074c1cb3fbdde886696330bb8d4b2
f.minlos <f.minlos@gmail.com>
2019-07-28 08:53:49 +0300

    order titles

--- documents.py
+++ documents.py
@@ -1,6 +1,7 @@
 import codecs
 import os
 import re
+import dateparser
 from bs4 import BeautifulSoup, element
 import xml.etree.ElementTree as ET
 from utils import clean_text
@@ -40,10 +41,12 @@ class Document():
         self.__tree = None
         self.title = None
         self.clean_title = None
+        self.order_titles = []
         self.get_soup()
         self.get_title()
         self.get_text()
         self.is_amendment = self.clean_title.startswith(u"изменен и дополнен")
+        self.is_order = self.clean_title.startswith(u'приказ')
         if self.is_amendment:
             spl = re.split(u' к|во? ', self.clean_title)
             if len(spl)>1:
@@ -52,19 +55,48 @@ class Document():
                 self.main_doc_title = None
         else:
             self.main_doc_title = None
+        if self.is_order:
+            self.get_order_titles()
         self.incoming_links = []
         # self.links = list(self.all_links())
         self.soup = None # the soup object is not trivially pickled because of the recursion, so we'd better null it
+
        
     def __repr__(self):
         return "<Document: path: {path}; folder {f}; html_file: {html}; title: {t}; processed title: {ct}>".format(f=self.folder, t=self.title, ct=self.clean_title, html=self.html_file, path=os.path.join(self.folder, self.html_file))
+
+
+    def get_order_titles(self):
+        self.order_titles = []
+        num, date = self.extract_params()
+        if num and date:
+            for bank in ("Банка", "ОАО Банк ВТБ", ""):
+                title = "Приказ {bank} от {date} № {num}".format(bank=bank, date=date, num=num)
+                self.order_titles.append(title)
+        print (self.order_titles)
+
+    def extract_params(self):
+        """extract date and number"""
+        order_num = None
+        formatted_date = None
+        spl = self.title.split("№")
+        if len(spl)==2:
+            order_num = spl[1]
+        if order_num:
+            for elem in self.soup.find_all('p'):
+                if order_num in elem.text:
+                    date= re.split(order_num, elem.text)[0]
+                    parsed_date = dateparser.parse(date.strip())
+                    if parsed_date is not None:
+                        formatted_date = parsed_date.date().isoformat()
+        print (order_num, formatted_date)
+        return (order_num, formatted_date)
 
     def get_soup(self):
         if not self.soup:
             fullname = os.path.join(FOLDER, self.folder, self.html_file)
             doc = codecs.open(fullname).read()
             self.soup = BeautifulSoup(doc, "html.parser")
-
 
     @property
     def properties_tree(self):
